81c9b282185c1207da3aa802f1b785544911a2e4
Roman Alikevich <roma.alikevich8@gmail.com>
2023-10-22 20:55:44 +0300

    dsdsf

--- task8/ContentView.swift
+++ task8/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
         UIScreen.main.bounds.size.height * 0.25
     }
     
-    @State private var yOffset: CGFloat = 0.3
+    @State private var yOffset: CGFloat = 0.5
 //    @State private var xOffset: CGFloat = 1
     
     private var anchor: UnitPoint {
@@ -34,34 +34,34 @@ struct ContentView: View {
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .edgesIgnoringSafeArea(.all)
-                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .frame(width: UIScreen.main.bounds.width,
+                       height: UIScreen.main.bounds.height)
                 .background(Color.clear)
-            let view = Rectangle()
-                .frame(width: width, height: height)
+            Rectangle()
+//                .frame(width: width, height: height)
                 .background(.ultraThinMaterial)
-                .cornerRadius(15)
+                .cornerRadius(10)
                 .scaleEffect(x: yOffset >= 1 || yOffset <= 0 ? 0.85 : 1,
-                             y: yOffset >= 1 || yOffset <= 0 ? 1.05 : 1,
+                             y: yOffset >= 1 || yOffset <= 0 ? 1.1 : 1,
                              anchor: anchor)
-                .animation(.spring(dampingFraction: 0.5), value: yOffset)
-            view.overlay(alignment: .bottom) {
+                .animation(.spring(dampingFraction: 0.7), value: yOffset)
+            .overlay(alignment: .bottom) {
                 let view = Rectangle()
                 view
-//                    .frame(height: max(0, height))
-//                    .frame(width: width, height: height * yOffset)
                     .scaleEffect(y: yOffset, anchor: .bottom)
                     .cornerRadius(10)
                     .foregroundColor(.white)
-                    .gesture(
-                        DragGesture()
-                            .onChanged { value in
-                                yOffset = max(min((1 - value.location.y / height), height), 0)
-                            }
-                            .onEnded { _ in
-                                yOffset = 0.3
-                            }
-                    )
-            }//.frame(width: width, height: height)
+                    
+            }.frame(width: width, height: height)
+                .gesture(
+                    DragGesture()
+                        .onChanged { value in
+                            yOffset = max(min((1 - value.location.y / height), height), 0)
+                        }
+                        .onEnded { _ in
+                            yOffset = 0.5
+                        }
+                )
         }
     }
 }
